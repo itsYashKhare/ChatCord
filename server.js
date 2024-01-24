d7fb94ddd9dadf60f5467446de923cb600ec1cd8
itsYashKhare <kharey02@gmail.com>
2024-01-24 23:51:35 +0530

    sovled the error

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
-const socketio = require("socket.io")
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 //setting static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Running when client connects
-io.on("connection", socket => {
-    console.log("New WebSocket Connection...");
+io.on("connection", (socket) => {
+  // console.log("New WebSocket Connection...");
 
-    socket.emit("message","Welcome to ChatCord!");
+  // Welcome current user
+  socket.emit("message", "Welcome to ChatCord!");
 
+  // Broadcast when the  user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // Runs when clients disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user left the chat");
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
+const PORT = 3000 || process.env.PORT;
 
-const PORT =  3000 || process.env.PORT;
-
-server.listen(PORT,()=> console.log(`Server running on ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
