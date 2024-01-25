f8b3399a78f74e7c6250f7e4eb1504708ad51b9b
itsYashKhare <kharey02@gmail.com>
2024-01-25 23:59:50 +0530

    added chat feature

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,24 +11,33 @@ const io = socketio(server);
 //setting static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatCord Bot";
+
 // Running when client connects
 io.on("connection", (socket) => {
   // console.log("New WebSocket Connection...");
 
+  socket.on("joinroom" , ({ username, room}) => {
+
+  });
+
   // Welcome current user
-  socket.emit("message", "Welcome to ChatCord!");
+  socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
 
   // Broadcast when the  user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
 
   // Runs when clients disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user left the chat");
+    io.emit("message", formatMessage(botName, "A user left the chat"));
   });
 
   // Listen for chatMessage
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
